f78a1332e070bf071abfb3b0a50d86e150f9939a
Code36u4r60 <code36u4r60@hotmail.com>
2024-02-04 22:00:43 +0000

    fix: change type uuid to text

--- database/migrations/20240201155701_create-statements.ts
+++ database/migrations/20240201155701_create-statements.ts
@@ -4,11 +4,9 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.createTable('statements', table => {
         table.uuid('id').primary()
-        
-        table.uuid('user_id').notNullable().index()
+        table.text('user_id').notNullable().index()
         table.text('option').notNullable()
         table.date('date').notNullable().index()
-
         table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
     })
 }
--- database/migrations/20240201160903_create-authentication.ts
+++ database/migrations/20240201160903_create-authentication.ts
@@ -4,7 +4,7 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.createTable('authentication', table => {
         table.uuid('id').primary()
-        table.uuid('user_id').notNullable()
+        table.text('user_id').notNullable()
         table.text('token').notNullable().unique()
         table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
     })
