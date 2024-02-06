08a05d6defc0e80731dbd977984390d6ab511d3a
Code36u4r60 <code36u4r60@hotmail.com>
2024-02-06 14:40:47 +0000

    wip

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,6 @@ import { app } from './app'
 app
   .listen({
     port: env.PORT,
-    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
+    host: 'RENDER' in process.env ? '0.0.0.0' : '0.0.0.0',
   })
   .then(() => console.log(`Server is running on port ${env.PORT}`));
